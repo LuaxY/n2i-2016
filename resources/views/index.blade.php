f3a74a8f1ec0dc22282f3d4596b306b12044c33a
Thomas Burgio <Thomas Burgio>
2016-12-02 00:18:48 +0100

    add template header

---
+++ resources/views/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Connexion</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="../assets/css/app.css" rel="stylesheet" type="text/css">
+        <link href="../assets/css/skeleton.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="header">
+          <h1>CONNEXION AU PARTAGE</h1>
+        </div>
+        <div class="container">
+          <form method="POST" action="">
+            <div class="row">
+              <div class="six columns">
+                <label for="mailInput">Your email</label>
+                <input class="u-full-width" type="email" placeholder="email@operator.com" id="mailInput">
+              </div>
+              <div class="six columns">
+                <label for="passwordInput">Your password</label>
+                <input class="u-full-width" type="password" placeholder="*********" id="passwordInput">
+              </div>
+            </div>
+            <input type="submit" value="Connexion">
+          </form>
+
+          <div class="line-separator"></div>
+
+          <p>Vous n'êtes pas inscrit ?</p>
+        </div>
+    </body>
+</html>
