fd8bc2755283e819561ac03aeb1e55be99d5417b
Saïd Mezhoud <said.mezhoud@voidmx.net>
2016-12-02 00:53:54 +0100

    redefine view route

---
+++ resources/views/login.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Connexion</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link href="../resources/assets/css/app.css" rel="stylesheet" type="text/css">
+    <link href="../resources/assets/css/skeleton.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="header">
+    <h1>CONNEXION AU PARTAGE</h1>
+</div>
+<div class="container">
+    <form method="POST" action="">
+        <div class="row">
+            <h3 class="center">Se connecter</h3>
+            <div class="twelve columns">
+                <label for="mailInput">Email</label>
+                <input class="u-full-width" type="email" placeholder="email@operator.com" id="mailInput">
+            </div>
+            <div class="twelve columns">
+                <label for="passwordInput">Mot de Passe</label>
+                <input class="u-full-width" type="password" placeholder="*********" id="passwordInput">
+            </div>
+        </div>
+        <div class="center" style="margin-top:35px;"><input type="submit" value="Connexion"></div>
+    </form>
+</div>
+
+<div class="footer">
+    <p>test</p>
+</div>
+</body>
+</html>
