800c3c6f0ba22381ab49e0d1e89617b0d55938fe
Saïd Mezhoud <said.mezhoud@voidmx.net>
2016-12-02 03:43:35 +0100

    list item formations

--- resources/views/formation/list.blade.php
+++ resources/views/formation/list.blade.php
@@ -2,13 +2,13 @@
 @section('title') Formations @endsection
 @section('content')
 <h1 style="padding:10px;">FORMATIONS</h1>
-<?php
-  for ($i=0; $i < 5 ; $i++) {
-    echo'
-      <div class="item">
-        <h4>FormationTitle'.$i.'</h4>
-      </div>
-    ';
-  }
-?>
+@if (empty($formations))
+    Aucun post n'est disponible... Veuillez réessayer ultérieurement.
+@else
+    @foreach ($formations as $array)
+        <div class="item">
+            <h4>{{ $array->title }}</h4>
+        </div>
+    @endforeach
+@endif
 @endsection
