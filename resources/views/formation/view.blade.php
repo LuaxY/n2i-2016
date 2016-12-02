001458bcb2d4805f48d745790adcef5e886f65d3
LuaxY <yann.guineau@voidmx.net>
2016-12-02 04:37:38 +0100

    Display search & add pages list

--- resources/views/formation/view.blade.php
+++ resources/views/formation/view.blade.php
@@ -2,7 +2,19 @@
 @section('title') {{ $formation->title }} @endsection
 @section('content')
     <div class="center"><h1 style="padding:10px;">{{ $formation->title }}</h1></div>
+    <div class="search">
+      <form class="" action="{{ URL::route('search') }}" method="post">
+          <p style="display:inline;">Recherche: </p><input type="text" name="query" value="">
+      </form>
+    </div>
     <div class="itemArticle">
         {{ $formation->description }}
     </div>
+    @foreach ($formation->pages()->get() as $page)
+        <div class="item">
+            <a href="{{ route('page.view', [$formation->id, $page->id]) }}">
+                <h4>{{ $page->title }}</h4>
+            </a>
+        </div>
+    @endforeach
 @endsection
